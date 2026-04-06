0223cb407f154cf83db2ce4c1050b2db8a4b3862
wesley.araujo <wesley.araujo@sicoob.com.br>
2026-04-06 18:59:23 -0300

    complementando os endpoints

--- src/main/java/com/ojuara/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/ojuara/helpdesk/resources/TecnicoResource.java
@@ -1,9 +1,14 @@
 package com.ojuara.helpdesk.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ojuara.helpdesk.model.Tecnico;
 import com.ojuara.helpdesk.model.dto.TecnicoDto;
@@ -11,6 +16,8 @@ import com.ojuara.helpdesk.services.TecnicoService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(value = "/tecnicos")
@@ -23,6 +30,22 @@ public class TecnicoResource {
     public ResponseEntity<TecnicoDto> findById(@PathVariable Integer id) {
         Tecnico obj = service.findById(id);
         return ResponseEntity.ok().body(new TecnicoDto(obj));
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TecnicoDto>> findAll() {
+        List<Tecnico> list = service.findAll();
+        List<TecnicoDto> listDto = list.stream().map(obj -> new TecnicoDto(obj)).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(listDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<TecnicoDto> create(@RequestBody TecnicoDto tecnicoDto){
+        Tecnico newObj = service.create(tecnicoDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
 
     }
 
--- src/main/java/com/ojuara/helpdesk/services/TecnicoService.java
+++ src/main/java/com/ojuara/helpdesk/services/TecnicoService.java
@@ -1,11 +1,13 @@
 package com.ojuara.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ojuara.helpdesk.model.Tecnico;
+import com.ojuara.helpdesk.model.dto.TecnicoDto;
 import com.ojuara.helpdesk.repositories.TecnicoRepository;
 import com.ojuara.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -18,6 +20,21 @@ public class TecnicoService {
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objecto não encontrado Id:" + id));
+    }
+
+    public List<Tecnico> findAll() {
+        List<Tecnico> list = repository.findAll();
+        if (list.isEmpty()) {
+            throw new ObjectNotFoundException("Nenhum técnico encontrado");
+        }
+        return list;
+    }
+
+    public Tecnico create(TecnicoDto tecnicoDto) {
+        tecnicoDto.setId(null); //assegurando que é uma nova requisição
+        Tecnico newObj = new Tecnico(tecnicoDto);
+        return repository.save(newObj);
+
     }
 
 }
